46bfe7dab22ecd00872268745c4e0894a4259fa6
laggedskapari <es.rajatyadav@gmail.com>
2024-06-13 13:58:44 +0530

    Variable: basic types.

--- go_boot-dev/day_1.go
+++ go_boot-dev/day_1.go
@@ -2,51 +2,47 @@ package main
 
 import "fmt"
 
-//func main() {
-//  fmt.Println("Hello World!")
-//}
-
-// func main(){
-//   messagesFromDoris := []string {
-//     "You doing anything later??",
-//     "Did you get my last message?",
-//     "Don't leave me hanging...",
-//     "Please respond I'm lonely!",
-//   }
-//
-//   numMessages := float64(len(messagesFromDoris))
-//   costPerMessage := .02
-//
-//   totalCost := costPerMessage * numMessages
-//
-//   fmt.Printf("Doris spent %.2f on text messages today \n", totalCost)
-// }
-
-// func main() {
-//   fmt.Println("the compiled textio server is starting")
-// }
-
-// func main() {
-//   var username string = "presidentSkroob"
-//   var password string = "12345"
-//
-//   fmt.Println("Authentication: Basic", username+":"+password)
-// }
-
-// func main() {
-// 	var startup string = "Textio SMS service booting up..."
-// 	var message string = "Sending text message"
-// 	var confirm string = "Message sent!"
-//
-// 	fmt.Println(confirm)
-// 	fmt.Println(message)
-// 	fmt.Println(startup)
-// }
-
 func main() {
-  var senderName string = "Syl"
-  var recipient string = "Kaladin"
-  var message string = "The words, Kaladin. You have to speak the Words!"
+	//   fmt.Println("Hello World!")
 
-  fmt.Printf("%s to %s: %s\n", senderName, recipient, message);
+	//   messagesFromDoris := []string {
+	//     "You doing anything later??",
+	//     "Did you get my last message?",
+	//     "Don't leave me hanging...",
+	//     "Please respond I'm lonely!",
+	//   }
+	//   numMessages := float64(len(messagesFromDoris))
+	//   costPerMessage := .02
+	//
+	//   totalCost := costPerMessage * numMessages
+	//
+	//   fmt.Printf("Doris spent %.2f on text messages today \n", totalCost)
+
+	//   fmt.Println("the compiled textio server is starting")
+
+	//   var username string = "presidentSkroob"
+	//   var password string = "12345"
+	//
+	//   fmt.Println("Authentication: Basic", username+":"+password)
+
+	// 	var startup string = "Textio SMS service booting up..."
+	// 	var message string = "Sending text message"
+	// 	var confirm string = "Message sent!"
+	//
+	// 	fmt.Println(confirm)
+	// 	fmt.Println(message)
+	// 	fmt.Println(startup)
+
+	//	  var senderName string = "Syl"
+	//	  var recipient string = "Kaladin"
+	//	  var message string = "The words, Kaladin. You have to speak the Words!"
+	//
+	//	  fmt.Printf("%s to %s: %s\n", senderName, recipient, message);
+
+  var smsSendingLimit int
+  var costPerSMS float64
+  var hasPermission bool
+  var username string
+
+  fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
