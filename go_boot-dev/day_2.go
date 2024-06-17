8b45542ecf93e7728657992a49258ac983af486b
laggedskapari <es.rajatyadav@gmail.com>
2024-06-18 01:41:42 +0530

    Functions -> Introduction

--- go_boot-dev/day_2.go
+++ go_boot-dev/day_2.go
@@ -1,23 +1,23 @@
-package main 
-
-import "fmt"
-
-func main() {
-  var insufficientFundMessage string = "Purchase failed. Insufficient funds."
-  var purchaseSuccessMessage string = "Purchase successful."
-  var accountBalance float64 = 100.0
-  var bulkMessageCost float64 = 75.0
-  var isPremiumUser bool = true
-  var discountRate float64 = 0.10
-  var finalCost float64 = bulkMessageCost
-
-  if isPremiumUser {
-    finalCost -= finalCost * discountRate
-  }
-
-  if finalCost <= accountBalance {
-    fmt.Println(purchaseSuccessMessage)
-  } else {
-    fmt.Println(insufficientFundMessage)
-  }
-}
+// package main 
+//
+// import "fmt"
+//
+// func main() {
+//   var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+//   var purchaseSuccessMessage string = "Purchase successful."
+//   var accountBalance float64 = 100.0
+//   var bulkMessageCost float64 = 75.0
+//   var isPremiumUser bool = true
+//   var discountRate float64 = 0.10
+//   var finalCost float64 = bulkMessageCost
+//
+//   if isPremiumUser {
+//     finalCost -= finalCost * discountRate
+//   }
+//
+//   if finalCost <= accountBalance {
+//     fmt.Println(purchaseSuccessMessage)
+//   } else {
+//     fmt.Println(insufficientFundMessage)
+//   }
+// }
