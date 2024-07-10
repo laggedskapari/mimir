bb88cf6c03801ef531d9a5196f583faab81ea217
laggedskapari <es.rajatyadav@gmail.com>
2024-07-10 09:37:26 +0530

    Struct: Embedded Struct

--- go_boot-dev/message_to_send_test.go
+++ go_boot-dev/message_to_send_test.go
@@ -1,56 +1,61 @@
-package main
-
-import (
-	"fmt"
-	"testing"
-)
-
-func getMessageText(m messageToSend) string {
-	return fmt.Sprintf("Sending message: '%s' to: %v\n", m.message, m.phoneNumber)
-}
-
-func Test(t *testing.T) {
-	type testCase struct {
-		phoneNumber int
-		message     string
-		expected    string
-	}
-	tests := []testCase{
-		{148255510981, "Thanks for signing up", "Sending message: 'Thanks for signing up' to: 148255510981\n"},
-		{148255510982, "Love to have you aboard!", "Sending message: 'Love to have you aboard!' to: 148255510982\n"},
-	}
-	if withSubmit {
-		tests = append(tests, []testCase{
-			{148255510983, "We're so excited to have you", "Sending message: 'We're so excited to have you' to: 148255510983\n"},
-			{148255510984, "", "Sending message: '' to: 148255510984\n"},
-			{148255510985, "Hello, World!", "Sending message: 'Hello, World!' to: 148255510985\n"},
-			{148255510986, "This is a test message", "Sending message: 'This is a test message' to: 148255510986\n"},
-		}...)
-	}
-
-	for _, test := range tests {
-		if output := getMessageText(messageToSend{
-			phoneNumber: test.phoneNumber,
-			message:     test.message,
-		}); output != test.expected {
-			t.Errorf(
-				"Test Failed: (%v, %v) -> expected: %v actual: %v",
-				test.phoneNumber,
-				test.message,
-				test.expected,
-				output,
-			)
-		} else {
-			fmt.Printf("Test Passed: (%v, %v) -> expected: %v actual: %v\n",
-				test.phoneNumber,
-				test.message,
-				test.expected,
-				output,
-			)
-		}
-	}
-}
-
-// withSubmit is set at compile time depending
-// on which button is used to run the tests
-var withSubmit = true
+// package main
+//
+// import (
+// 	"fmt"
+// 	"testing"
+// )
+//
+// func Test(t *testing.T) {
+// 	type testCase struct {
+// 		mToSend  messageToSend
+// 		expected bool
+// 	}
+// 	tests := []testCase{
+// 		{messageToSend{
+// 			message:   "you have an appointment tomorrow",
+// 			sender:    user{name: "Brenda Halafax", number: 16545550987},
+// 			recipient: user{name: "Sally Sue", number: 19035558973},
+// 		}, true},
+// 		{messageToSend{
+// 			message:   "you have an event tomorrow",
+// 			sender:    user{number: 16545550987},
+// 			recipient: user{name: "Suzie Sall", number: 19035558973},
+// 		}, false},
+// 	}
+// 	if withSubmit {
+// 		tests = append(tests, []testCase{
+// 			{messageToSend{
+// 				message:   "you have an birthday tomorrow",
+// 				sender:    user{name: "Jason Bjorn", number: 16545550987},
+// 				recipient: user{name: "Jim Bond"},
+// 			}, false},
+// 			{messageToSend{
+// 				message:   "you have a party tomorrow",
+// 				sender:    user{name: "Njorn Halafax"},
+// 				recipient: user{name: "Becky Sue", number: 19035558973},
+// 			}, false},
+// 			{messageToSend{
+// 				message:   "you have a birthday tomorrow",
+// 				sender:    user{name: "Eli Halafax", number: 16545550987},
+// 				recipient: user{number: 19035558973},
+// 			}, false},
+// 		}...)
+// 	}
+//
+// 	for _, test := range tests {
+// 		if output := canSendMessage(test.mToSend); output != test.expected {
+// 			t.Errorf(
+// 				"Test Failed: (%+v) -> expected: %v actual: %v",
+// 				test.mToSend,
+// 				test.expected,
+// 				output,
+// 			)
+// 		} else {
+// 			fmt.Printf("Test Passed: (%+v) -> expected: %v actual: %v\n", test.mToSend, test.expected, output)
+// 		}
+// 	}
+// }
+//
+// // withSubmit is set at compile time depending
+// // on which button is used to run the tests
+// var withSubmit = true
