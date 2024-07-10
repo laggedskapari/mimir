ac742de2e8df9cb8d0bd4ad618cb388ca72fd97a
laggedskapari <es.rajatyadav@gmail.com>
2024-07-10 11:25:26 +0530

    Struct: struct methods in Go

--- go_boot-dev/struct_rate_limit.go
+++ go_boot-dev/struct_rate_limit.go
@@ -1,11 +1,11 @@
-package main
-
-type sender struct {
-  rateLimit int
-  user
-}
-
-type user struct {
-  name string 
-  number int
-}
+// package main
+//
+// type sender struct {
+//   rateLimit int
+//   user
+// }
+//
+// type user struct {
+//   name string 
+//   number int
+// }
