35081ae6c8f04bcccc10ff79acf96355880f5470
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 11:34:29 +0530

    Comparisons: Ternary

---
+++ bootdotdev/javascripto/comparisons/comparisons_ternary.js
@@ -0,0 +1,10 @@
+const bobbyAge = 17
+const legalAge = 18
+
+// don't touch above this line
+
+const message = bobbyAge > legalAge ? 'Bobby is old enough' : 'Bobby is not old enough';
+
+// don't touch below this line
+
+console.log(message)
