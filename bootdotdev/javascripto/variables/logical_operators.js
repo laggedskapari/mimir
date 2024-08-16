2807771916b7652b24de6cfbffefe834286980ea
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 11:01:16 +0530

    Variables: Operators

---
+++ bootdotdev/javascripto/variables/logical_operators.js
@@ -0,0 +1,13 @@
+const isBigHit = true
+const isNew = true
+const hasAwards = false
+const canHaveSequel = true
+const isRatedX = false
+
+// don't touch above this line
+
+const isBlockBuster = isBigHit && isNew && (hasAwards || canHaveSequel) && !isRatedX
+
+// don't touch below this line
+
+console.log(`The movie is a blockbuster: ${isBlockBuster}`)
