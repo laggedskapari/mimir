48376b18493816b4474c6d4448f79c0e756c58d1
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 22:42:03 +0530

    Array: Length

---
+++ bootdotdev/javascripto/arrays/array_lenght.js
@@ -0,0 +1,26 @@
+const getMostRecentUser = (usernames) => {
+  return usernames.length ? usernames[usernames.length - 1] : null
+}
+
+// don't touch below this line
+
+console.log(`Most recent user: ${getMostRecentUser(
+  []
+)}`)
+
+console.log(`Most recent user: ${getMostRecentUser(
+  [
+    'johndoe123',
+    'billyrae456'
+  ]
+)}`)
+
+console.log(`Most recent user: ${getMostRecentUser(
+  [
+    'wagslane',
+    'jimmyjohn',
+    'bopeep',
+    'strightkilla',
+    'reddyman'
+  ]
+)}`)
