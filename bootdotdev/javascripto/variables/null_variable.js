f9b93e43aff3d92c7d42ad569bea4a692f441851
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 11:01:33 +0530

    Variables: Null

---
+++ bootdotdev/javascripto/variables/null_variable.js
@@ -0,0 +1,9 @@
+const movieTitle = null
+
+// don't touch below this line
+
+// The 'not' operator implicitly
+// casts movieTitle to a boolean value
+const movieHasNoTitle = !movieTitle
+
+console.log(`The movie does not have a title: ${movieHasNoTitle}`)
