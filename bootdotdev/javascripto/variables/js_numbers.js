263e59a4cd66953efff12b370d8faf74ff2a4e87
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 11:00:25 +0530

    Variables: Numbers

---
+++ bootdotdev/javascripto/variables/js_numbers.js
@@ -0,0 +1,18 @@
+const ironManStars = 4
+const thorStars = 5
+const captainAmericaStars = 3
+const avengerStars = 1
+const hulkStars = 2
+
+// don't touch above this line
+
+const mcuTotalStars = ironManStars + thorStars + captainAmericaStars + hulkStars
+const mcuAverageStars = (ironManStars + thorStars + captainAmericaStars + hulkStars) / 5
+
+
+
+
+// don't touch below this line
+
+console.log(mcuTotalStars)
+console.log(mcuAverageStars)
