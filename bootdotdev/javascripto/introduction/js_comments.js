284ef59821016d7a2aea8fab7b818b86c0fc6291
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 10:59:52 +0530

    Introduction: Comments

---
+++ bootdotdev/javascripto/introduction/js_comments.js
@@ -0,0 +1,10 @@
+//This prints the name of the server.
+console.log('Starting MovieStarz server.')
+
+/* 
+ Below we print 
+ the name of the 
+ first user of MovieStarz application
+*/
+
+console.log('John Doe')
