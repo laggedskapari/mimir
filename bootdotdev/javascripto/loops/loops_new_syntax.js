cb4e43bff826b1107db8fefb14a855cbf49e10c4
laggedskapari <es.rajatyadav@gmail.com>
2024-08-17 13:03:26 +0530

    Loops: New syntax

---
+++ bootdotdev/javascripto/loops/loops_new_syntax.js
@@ -0,0 +1,15 @@
+const printCleanReviews = (reviews, badWord) => {
+  for (let review of reviews) {
+    if (review.includes(badWord)) {
+      continue
+    }
+    console.log(review)
+  }
+}
+
+// don't touch below this line
+
+printCleanReviews([ 'The movie sucks', 'I love it', 'What garbage', 'so sucky' ], 'suck')
+console.log('---')
+printCleanReviews([ 'The movie sucks', 'I love it', 'What darn crap', 'darn, so sucky' ], 'darn')
+console.log('---')
